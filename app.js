43308fd84b2e8f6ac2189b9714f21fc8fa66d495
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-05-17 15:33:41 +0300

    function that returns date and time nicely

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+"use strict"; // here we go again
+function getTime() {
+  const now = new Date();
+  const dataString = now.toLocaleDateString();
+  const timeString = now.toLocaleTimeString();
 
+  return dataString + " " + timeString;
+}
+getTime();
