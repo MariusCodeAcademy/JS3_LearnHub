1f2282cd0c85e602ab9d412c6fdf2d1594d4a99a
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-05-17 14:42:02 +0300

    initial commit

---
+++ uzd.js
@@ -0,0 +1,62 @@
+/**
+ * Git practice (visas naujas sakas kuriam is master)
+ *
+ * 1 sukurti index html faila, inicijuoti git
+ *
+ * 2. sukriti app.js ir style.css  ir prideti visus failus kaip "init commit "
+ *
+ * 3. susikurti nauja branch html. ir dirbam tame branch
+ * sukurti html struktura ir prjungti js ir css failus
+ * // sukurti h1 antraste ir h4 mazesne antraste
+ *
+ * 4. prijungti html saka prie pagrindines master sakos (merge)
+ *
+ * 5. kurti nauja saka style ir joje prideti matomo stilau savo html elementams
+ *
+ * 6. Patvirtinti css pakeitimus su commit ir atitinkama zinute
+ *
+ * 7. prijungti style saka prie pagrindines
+ *
+ * 8. kurti nauja saka "time"
+ *
+ * 9. toje sakoje su js sukurti h4 elementa ir i ji ikelti data ir laika is js.
+ * ikelti naujai sukurta h4 elmenta su append i musu index.html dokumenta
+ *
+ * 10. prijungti "time" saka prie pagrindines
+ *
+ * 11. sugryzti laike i 4tu uzduoties punktu pazymeta commit'a
+ * is ten susikurti nauja saka "after html" (skaityti instrukcija po detach HEAD )
+ *
+ * 12. gryzti i master saka
+ *
+ * 13. Sukurti saka "page" ir joje sukurti headeri, footeri, pora post articles su nuotraukom
+ * commitini (tvirtinti) po kievieno el sukurimu
+ *
+ * 14. sustilizuoti elmentus ir pakomintinti tiek kiekvieno el stilium
+ *
+ * 15. prijungti "page" saka prie pagrindines
+ *
+ * 16. Susikurti saka 'aboutPage'
+ *
+ * 17. 'aboutPage' sakoj tvarkingai commitinant sukurti siek tiek html, prideti stiliu
+ * ir parsiusti random juokeli is chuck noris ar kito api. https://api.chucknorris.io/
+ *
+ * 18. prijungti 'aboutPage' prie master
+ *
+ * 19. sukrti saka 'darkMode'
+ * toje sakoje stiliaus faile klasiu pagalba paversti viesias spalvas tamsiom
+ * ir tamsias sviesiom. (naktinis rezimas )
+ *
+ * 20. sukurti saka 'article' joje sukurti secion su antraste nuotrauka ir keletu pastraipu,
+ * sustilizuoti kad atrodytu nice. visa procesa commintinti ties loginiais zingsniais
+ *
+ * 21. prijungti 'article' saka prie pagrindines sakos
+ *
+ * 22. pagrindinej sakoj js faile parasyti funkcija kuri parsiuncia 10 todo ir jsonPlaceholder
+ * api ir juos atvaizduoti saraso pavidalu
+ *
+ *
+ * 23. paskaityti kaip veikia revert ir pabandyti parevertinti kazkuri matomo stiliaus commit
+ *
+ *
+ */
